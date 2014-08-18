4cf6fc3d3380c1af6021c481995fbeeb37a46ede
Dmitry Pustovit <me@dmitrypustovit.com>
2014-08-18 16:35:24 -0400

    Site

--- scripts/barrelroll.js
+++
@@ -1,4 +0,0 @@
-function barrelroll() {
-	$('#roll').hide();
-	$('#rolltext').show();
-}
--- scripts/widthfix.js
+++
@@ -1,9 +0,0 @@
-$(window).resize(function() {
-	if (screen.width < 1024) {
-        $(".scrollToTop").hide();
-    }
-    else {
-
-        $(".scrollToTop").show();
-    }
-});
